25ad3d696bd2584b295ebf1961e7549f718e9aa4
ToufikOnTheFlux <98532736+ToufikOnTheFlux@users.noreply.github.com>
2022-01-28 20:28:57 +0100

    Update Main.php

--- QuasarCommand/src/Command/Main.php
+++ QuasarCommand/src/Command/Main.php
@@ -2,39 +2,50 @@
 
 namespace Command;
 
-use pocketmine\block\Block;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\data\bedrock\EffectIds;
-use pocketmine\entity\effect\Effect;
 use pocketmine\event\Listener;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\entity\effect\EffectInstance;
+use pocketmine\item\ItemFactory;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase implements Listener
+{
     public function onEnable(): void
     {
         $this->getLogger()->notice("Enable");
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
-{
-    $commandname = $command->getName();
-    if($commandname == "heal") {
-        if ($sender instanceof Player) {
-            $sender->sendMessage("§3Tu a été heal");
-            $sender->getHealth();
-            $sender->setHealth(20);
+    {
+        $commandname = $command->getName();
+        if ($commandname == "heal") {
+            if ($sender instanceof Player) {
+                $sender->sendMessage("§3Tu a été heal");
+                $sender->getHealth();
+                $sender->setHealth(20);
+            }
         }
-    }
-        if($commandname == "feed") {
+        if ($commandname == "feed") {
             if ($sender instanceof Player) {
                 $sender->getHungerManager()->setFood(20);
                 $sender->getHungerManager()->setSaturation(20);
                 $sender->sendMessage("§3Tu a bien été feed !");
             }
         }
+        if ($commandname == "speed") {
+            $sender->sendMessage("Speed activé !");
+            $sender->getMovementSpeed();
+            $sender->setMovementSpeed(4);
+            $sender->getInventory()->removeItem(ItemFactory::getInstance()->getItem(288));
+        }
+        if ($commandname == "uphealth") {
+            $sender->getMaxHealth();
+            $sender->setMaxHealth(40);
+            $sender->getHealth();
+            $sender->setHealth(40);
+            $sender->getArmorInventory()->removeItem(ItemFactory::getInstance()->get(306));
+        }
         return true;
-     }
+    }
 }
